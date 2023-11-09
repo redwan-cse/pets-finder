8305c8321fdb1ef49e32acb6b2ca3f110f494d9f
Md. Redwan Ahmed <redwanse@outlook.com>
2023-11-09 19:10:11 +0600

    Fixed Setup Error

--- src/pets/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Pet
-
-admin.site.register(Pet)
-
-admin.site.site_header = "Pet Finders Admin Panel"
-admin.site.site_title = "Pet Finders Admin Panel"
--- src/pets/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from pets.views import PetsListView, PetCreateView
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='index'),
-    path("all/", PetsListView.as_view(), name="pet_list"),
-    path("add/", PetCreateView.as_view(), name="pet_form"),
-]
