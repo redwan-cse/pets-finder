52cbd310dfd2c095c7289a38a94698fc4361b33f
Md. Redwan Ahmed <redwanse@outlook.com>
2023-11-11 00:27:04 +0600

    Sprint 1- add user login,registration,password reset pages

--- src/PetsFinderApp/urls.py
+++ src/PetsFinderApp/urls.py
@@ -1,6 +1,11 @@
 from . import views
-from django.urls import path
+from django.contrib import admin
+from django.urls import path,include
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("password_reset/", views.password_reset, name="password_reset"),
 ]
