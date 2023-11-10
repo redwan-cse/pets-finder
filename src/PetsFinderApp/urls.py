f1bee1a51482cd39424c313886fb6b078dc5af06
Md. Redwan Ahmed <redwanse@outlook.com>
2023-11-11 00:36:48 +0600

    Sprint 1- remove unnessary white space

--- src/PetsFinderApp/urls.py
+++ src/PetsFinderApp/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("", views.home, name="home"), 
-    path("register/", views.register, name="register"), 
-    path("login/", views.login, name="login"), 
-    path("logout/", views.logout, name="logout"), 
-    path("password_reset/", views.password_reset, name="password_reset"), 
+    path("", views.home, name="home"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("password_reset/", views.password_reset, name="password_reset"),
 ]
