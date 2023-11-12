882a67c170856101d35faf1d6aa05a96e5938c76
Md. Redwan Ahmed <redwanse@outlook.com>
2023-11-13 01:59:44 +0600

    remove python-decouple

--- src/PetsFinder/PetsFinder/settings.py
+++ src/PetsFinder/PetsFinder/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = "django-insecure-0e7-_y+=07(kmr91*btool9m^8@wcb2z-8_qv5#_eynp_86ro-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
