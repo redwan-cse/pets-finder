9992748f0b6ce433c5fead86d680227baa87687a
Md. Ziaur Rahman <mzr.csejnu@gmail.com>
2023-12-02 16:25:38 +0600

    model

--- src/PetsFinderApp/models.py
+++ src/PetsFinderApp/models.py
@@ -1,3 +1,13 @@
 # from django.db import models
 
+from django.db import models
+
 # Create your models here.
+
+class Student(models.Model):
+    name = models.Charfield(max_langth=200)
+    roll = models.Charfield(max_langth=10)
+
+
+    def_str_(self):
+            return self.name
