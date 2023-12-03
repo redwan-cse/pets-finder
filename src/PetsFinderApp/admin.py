b3d9659e94faee58253f54c6a0c7e127ada9f8e3
Md. Redwan Ahmed <redwanse@outlook.com>
2023-12-03 18:43:48 +0600

    Update admin.py

--- src/PetsFinderApp/admin.py
+++ src/PetsFinderApp/admin.py
@@ -1,3 +1,55 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Category, Pet, PetInstance, Owner
 
-# Register your models here.
+# Register the Admin class for Category using the decorator
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+# Register the Admin class for Pet using the decorator
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'description', 'microchip_id', 'display_breed')
+    search_fields = ('name', 'owner__last_name', 'owner__first_name', 'microchip_id', 'breed')
+    list_filter = ('category', 'owner')
+    ordering = ('name', 'category', 'owner')
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'category', 'owner', 'description', 'microchip_id')
+        }),
+        ('Breed', {
+            'fields': ('breed', 'display_breed')
+        }),
+    )
+
+# Register the Admin class for PetInstance using the decorator
+@admin.register(PetInstance)
+class PetInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'pet', 'breed', 'birth_date', 'status', 'available')
+    search_fields = ('id', 'pet__name', 'breed')
+    list_filter = ('status', 'available')
+    ordering = ('birth_date',)
+    
+    fieldsets = (
+        (None, {
+            'fields': ('pet', 'breed', 'birth_date')
+        }),
+        ('Availability', {
+            'fields': ('status', 'available')
+        }),
+    )
+
+# Register the Admin class for Owner using the decorator
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth')
+    search_fields = ('last_name', 'first_name')
+    ordering = ('last_name',)
+    
+    fieldsets = (
+        (None, {
+            'fields': ('first_name', 'last_name', 'date_of_birth')
+        }),
+    )
