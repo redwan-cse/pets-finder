e13d65dfb66ca24626bcf845595a11dfc833c956
Md. Redwan Ahmed <redwanse@outlook.com>
2023-12-03 19:17:06 +0600

    Update admin.py

--- src/PetsFinderApp/admin.py
+++ src/PetsFinderApp/admin.py
@@ -3,6 +3,7 @@
 from django.contrib import admin
 from .models import Category, Pet, PetInstance, Owner
 
+
 # Register the Admin class for Category using the decorator
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
